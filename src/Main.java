5938d5b5ebcab2a31ba70456799221a8d57bff08
ValeryB97 <valery.babaryko@gmail.com>
2024-04-25 18:23:14 +0700

    Homework 10 done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        // Задание 2
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        // Задание 3
+        String fullNameTrue = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф.И.О. сотрудника — " + fullNameTrue);
     }
 }
